5aa25e3ce59ff739b6546d6fa7feef2b5a5f6cf3
Muhammad Nazrul <74759104+nazrulshukri@users.noreply.github.com>
2026-01-21 11:38:07 +0800

    Update Hotel.js

--- nazrul/src/mockdata/Hotel.js
+++ nazrul/src/mockdata/Hotel.js
@@ -1,71 +1,35 @@
+// src/mockdata/hotelImages.js
 
+// Marriott
 import marriottImage from '../img/assets/selectedimage/marriotlist/448666213.jpg';
 import marriottImage1 from '../img/assets/selectedimage/marriotlist/king-superior-room.jpg';
 import marriottImage2 from '../img/assets/selectedimage/marriotlist/thomas-moore-meeting.jpg';
 import marriottImage3 from '../img/assets/selectedimage/marriotlist/front-view-of-the-hotel.jpg';
 import marriottImage4 from '../img/assets/selectedimage/marriotlist/lobby.jpg';
-import marriottImage5 from '../img/assets/selectedimage/marriotlist/the-den.jpg';
-import marriottImage6 from '../img/assets/selectedimage/marriotlist/20181218-063234-largejpg.jpg';
-import marriottImage7 from '../img/assets/selectedimage/marriotlist/lobby.jpg';
-import marriottImage8 from '../img/assets/selectedimage/marriotlist/lobby.jpg';
-import Sofitel from '../img/assets/selectedimage/Sofitel London St James/422088699.jpg'
-import Sofitel1 from '../img/assets/selectedimage/Sofitel London St James/443940309.jpg'
-import Sofitel2 from '../img/assets/selectedimage/Sofitel London St James/511969409.jpg'
-import Sofitel3 from '../img/assets/selectedimage/Sofitel London St James/511969411.jpg'
-import Sofitel4 from '../img/assets/selectedimage/Sofitel London St James/608585077.jpg'
-import citizenM from '../img/assets/selectedimage/citizenM/270976329.jpg'
-import citizenM1 from '../img/assets/selectedimage/citizenM/597738350.jpg'
-import citizenM2 from '../img/assets/selectedimage/citizenM/433628776.jpg'
-import citizenM3 from '../img/assets/selectedimage/citizenM/london-shoreditch-cm.jpg'
-import citizenM4 from '../img/assets/selectedimage/citizenM/photo4jpg.jpg'
-import sonder from '../img/assets/selectedimage/Henry Hotel/486092703.jpg'
-import sonder1 from '../img/assets/selectedimage/Henry Hotel/486092817.jpg'
-import sonder2 from '../img/assets/selectedimage/Henry Hotel/486092860.jpg'
-import sonder3 from '../img/assets/selectedimage/Henry Hotel/486092874.jpg'
-import sonder4 from '../img/assets/selectedimage/Henry Hotel/520519202.jpg'
-import Melia from '../img/assets/selectedimage/melia/121073066.jpg'
-import Melia1 from '../img/assets/selectedimage/melia/169024354.jpg'
-import melia2 from '../img/assets/selectedimage/melia/351964984.jpg'
-import melia3 from '../img/assets/selectedimage/melia/351966487.jpg'
-import melia4 from '../img/assets/selectedimage/melia/592838457.jpg'
-import pinancle from '../img/assets/selectedimage/pinancle/587773788.jpg'
-import pinancle1 from '../img/assets/selectedimage/pinancle/328780242.jpg'
-import pinancle2 from '../img/assets/selectedimage/pinancle/328780248.jpg'
-import pinancle3 from '../img/assets/selectedimage/pinancle/328780420.jpg'
-import pinancle4 from '../img/assets/selectedimage/pinancle/328780648.jpg'
-import myfair from '../img/assets/selectedimage/myfair/357727730.jpg'
-import myfair1 from '../img/assets/selectedimage/myfair/357727730.jpg'
-import myfair2 from '../img/assets/selectedimage/myfair/357727730.jpg'
-import myfair3 from '../img/assets/selectedimage/myfair/357727730.jpg'
-import myfair4 from '../img/assets/selectedimage/myfair/357727730.jpg'
-import ruben from '../img/assets/selectedimage/Ruben/220828102.jpg'
-import ruben1 from '../img/assets/selectedimage/Ruben/220828954.jpg'
-import ruben2 from '../img/assets/selectedimage/Ruben/92892076.jpg'
-import ruben3 from '../img/assets/selectedimage/Ruben/92892106.jpg'
-import ruben4 from '../img/assets/selectedimage/Ruben/86364506.jpg'
-import chesterfield from '../img/assets/selectedimage/chesterfield/590596236.jpg'
-import chesterfield1 from '../img/assets/selectedimage/chesterfield/149243357.jpg'
-import chesterfield2 from '../img/assets/selectedimage/chesterfield/149644538.jpg'
-import chesterfield3 from '../img/assets/selectedimage/chesterfield/149646749.jpg'
-import chesterfield4 from '../img/assets/selectedimage/chesterfield/13751220.jpg'
-import chesam2hotel from '../img/assets/selectedimage/New folder/MainPage.jpg'
-import chesam2hotel1 from '../img/assets/selectedimage/New folder/547650968.jpg'
-import chesam2hotel2 from '../img/assets/selectedimage/New folder/547650986.jpg'
-import chesam2hotel3 from '../img/assets/selectedimage/New folder/547651007.jpg'
-import chesam2hotel4 from '../img/assets/selectedimage/New folder/551972745.jpg'
-import hotel41 from '../img/assets/selectedimage/41/13750089.jpg'
-import hotel412 from '../img/assets/selectedimage/41/13882901.jpg'
-import hotel413 from '../img/assets/selectedimage/41/220805748.jpg'
-import hotel414 from '../img/assets/selectedimage/41/37874296.jpg'
-import hotel415 from '../img/assets/selectedimage/41/586123322.jpg'
-import botree from '../img/assets/selectedimage/Botree/499914088.jpg'
-import botree2 from '../img/assets/selectedimage/Botree/505773348.jpg'
-import botree3 from '../img/assets/selectedimage/Botree/505773500.jpg'
-import botree4 from '../img/assets/selectedimage/Botree/505774094.jpg'
-import botree5 from '../img/assets/selectedimage/Botree/505774444.jpg'
-// import intercontinentalImage from '../img/assets/Hotel List/the-kensington-exterior.jpg';
-// import intercontinentalImage1 from '../img/assets/Hotel List/the-kensington-hotel.jpg';
-// import intercontinentalImage2 from '../img/assets/Hotel List/the-kensington.jpg';
-// import intercontinentalImage3 from '../img/assets/Hotel List/westbury-hotel-kensington-1024x683.jpg';
 
+// Sofitel
+import Sofitel from '../img/assets/selectedimage/Sofitel London St James/422088699.jpg';
+import Sofitel1 from '../img/assets/selectedimage/Sofitel London St James/443940309.jpg';
+import Sofitel2 from '../img/assets/selectedimage/Sofitel London St James/511969409.jpg';
+import Sofitel3 from '../img/assets/selectedimage/Sofitel London St James/511969411.jpg';
+import Sofitel4 from '../img/assets/selectedimage/Sofitel London St James/608585077.jpg';
 
+// citizenM
+import citizenM from '../img/assets/selectedimage/citizenM/270976329.jpg';
+import citizenM1 from '../img/assets/selectedimage/citizenM/597738350.jpg';
+import citizenM2 from '../img/assets/selectedimage/citizenM/433628776.jpg';
+import citizenM3 from '../img/assets/selectedimage/citizenM/london-shoreditch-cm.jpg';
+import citizenM4 from '../img/assets/selectedimage/citizenM/photo4jpg.jpg';
+
+// ✅ grouped export
+export const hotelImages = {
+  marriott: [
+    marriottImage,
+    marriottImage1,
+    marriottImage2,
+    marriottImage3,
+    marriottImage4,
+  ],
+  sofitel: [Sofitel, Sofitel1, Sofitel2, Sofitel3, Sofitel4],
+  citizenM: [citizenM, citizenM1, citizenM2, citizenM3, citizenM4],
+};
